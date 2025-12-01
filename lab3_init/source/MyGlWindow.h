abcfed94a0249533cd431009001b76176f54a494
B_SS <seungu286@gmail.com>
2025-12-01 22:08:08 +0900

    .

--- lab3_init/source/MyGlWindow.h
+++ lab3_init/source/MyGlWindow.h
@@ -1,9 +1,10 @@
 //#define  FREEGLUT_LIB_PRAGMAS  0
-
+#define GLM_ENABLE_EXPERIMENTAL 
 #include <iostream>
 #include <GL/gl3w.h>
 #include <string>
 #include "Loader.h"
+#include"colorCube.h"
 
 
 class MyGlWindow {
@@ -11,14 +12,13 @@ public:
 	MyGlWindow(int w, int h);
 	void draw();
 private:
-
-	
+	std::unique_ptr<ColorCube> m_cube;	
 	std::unique_ptr<ShaderProgram> shaderProgram;
 
-	GLuint vaohandle;
 	int m_width;
 	int m_height;
 	
+	void initialize();
 	void setupBuffer();
 
 };
---
+++ lab3_init/source/colorCube.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <GL/gl3w.h>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+class ColorCube {
+private:
+    GLuint vaoHandle;
+    GLuint vbo_cube_vertices, vbo_cube_colors;
+    GLuint ibo_cube_elements;
+public:
+	void setup();
+    void draw();
+};
